8c3b66321c304bc5153e6c6e4281ec8b3b944c90
Yihua Zhang <michaelzhang2@msn.com>
2019-06-16 00:00:34 +0200

    added spinner component and title routing to collection page from collection preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match }) => (
   <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+    <h1
+      className='title'
+      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
+    >
+      {title.toUpperCase()}
+    </h1>
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
@@ -17,4 +23,4 @@ const CollectionPreview = ({ title, items }) => (
   </div>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
